084bc86612cd8caa39f60fe2a81addaade820672
Okaba Mac <markokaba99@gmail.com>
2019-02-22 13:01:33 +0100

    Create react app restructure my architecture added a nav component added node-sass

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar  = () => (
+    <nav>
+        <ul>
+            <li href='#'>Home</li>
+            <li href='#'>Gallery</li>
+            <li href='#'>Press</li>
+            <li href='#'>Contact</li>
+        </ul>
+    </nav>
+);
+export default Navbar;
