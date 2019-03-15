c91571d94324926024718558ee3ed01aaceea383
Okaba Mac <markokaba99@gmail.com>
2019-03-15 20:16:12 +0100

    Trying to resolve deployment

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
-import './styles/main.scss';
-import {HashRouter, Route, Switch} from 'react-router-dom';
-import Hero from './components/Hero';
-import Home from './components/Home';
-import Gallery from './components/Gallery';
-import Press from './components/Press';
-import Contact from './components/Contact';
-import ErrorPage from './components/ErrorPage';
-
-
+import React, { Component } from "react";
+import "./styles/main.scss";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Hero from "./components/Hero";
+import Home from "./components/Home";
+import Gallery from "./components/Gallery";
+import Press from "./components/Press";
+import Contact from "./components/Contact";
+import ErrorPage from "./components/ErrorPage";
 
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-      <div>
-       <Hero />
-       <Switch>
-            <Route exact path={process.env.PUBLIC_URL + '/'} component={Home} />
-            <Route exact path={process.env.PUBLIC_URL + '/gallery'} component={Gallery} />
-            <Route exact path={process.env.PUBLIC_URL + '/contact'} component={Contact} />
-            <Route exact path={process.env.PUBLIC_URL + '/press'} component={Press} />
-            <Route exact path={process.env.PUBLIC_URL + '*'} component={ErrorPage} />
-      </Switch>
-      </div>
+      <HashRouter basename={process.env.PUBLIC_URL}>
+        <div>
+          <Hero />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/gallery" component={Gallery} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/press" component={Press} />
+            <Route exact path="*" component={ErrorPage} />
+          </Switch>
+        </div>
       </HashRouter>
     );
   }
